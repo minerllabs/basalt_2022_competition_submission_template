50e8ab55ef201b8896ca0725af6c73a7f9ec0e3e
KarolisRam <karolis.ram@gmail.com>
2022-07-01 12:45:34 +0100

    add environment variables for evaluation stage

--- run.py
+++ run.py
@@ -4,11 +4,16 @@ import test_CreateVillageAnimalPen
 import test_FindCave
 import test_MakeWaterfall
 
+import os
+EVALUATION_STAGE = os.getenv('EVALUATION_STAGE', 'all')
+
 # Training Phase
-train.main()
+if EVALUATION_STAGE in ['all', 'training']:
+    train.main()
 
 # Testing Phase
-test_BuildVillageHouse.main()
-test_CreateVillageAnimalPen.main()
-test_FindCave.main()
-test_MakeWaterfall.main()
+if EVALUATION_STAGE in ['all', 'testing']:
+    test_BuildVillageHouse.main()
+    test_CreateVillageAnimalPen.main()
+    test_FindCave.main()
+    test_MakeWaterfall.main()
